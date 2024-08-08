6f9a0aba87305cf0b3cb72e7525f1b0865ceb8a7
AxelMatthew12 <143260705+AxelMatthew12@users.noreply.github.com>
2024-08-08 20:11:12 +0700

    Short modify on inaSystem + Login pages

--- Home/InaSystemCall.py
+++ Home/InaSystemCall.py
@@ -1,10 +1,35 @@
+class LoginDisplay():
+    def execute(self):
+        print("||    SELAMAT DATANG DI LOGIN PAGE    ||")
+        print("||               LOGIN                ||")
+        print("||             REGISTRASI             ||")
+        print("========================================")
+        inputLg= input("Jawaban: ")
+        
+
 class Perkenalan():
     def execute(self):
-        print("||-Hallo Kami adalah Home_Help Center")
-        print("||Kami dapat membantu anda untuk mengatur dan menjadwalkan setiap kebutuhan di dalam rumah")
-        print("|| Tentu dengan efesiensi dan Kemudahan penggunaan yang akan membantu anda untuk lebih")
-        print("|| Produktif untuk melakukan kegiatan di luar Rumah, Kami dapat membantunya :) ")
-        print("||")
+        print("\n")
+        print("|| --Hallo Kami adalah Home_Help Center--                                                   ||")
+        print("|| Kami dapat membantu anda untuk mengatur dan menjadwalkan setiap kebutuhan di dalam rumah ||")
+        print("|| Tentu dengan efesiensi dan Kemudahan penggunaan yang akan membantu anda untuk lebih      ||")
+        print("|| Produktif untuk melakukan kegiatan di luar Rumah, Kami dapat membantunya :)              ||")
+        print("||==========================================================================================||")
+        print("|| Lanjut ke next slide!. Berbagung dengan kami ")
+        print("|| (1.Next / 2.Stay)")
+        inputoption= input("Jawaban: ")
+
+        try:
+            inputoption = int(inputoption)
+        except ValueError:
+            inputoption = None
+        if inputoption == 1:
+            command=LoginDisplay()
+            command.execute()
+        elif inputoption ==2:
+            command= Perkenalan()
+            command.execute()
+
 
 def MainIna():
     print("||    SELAMAT DATANG DI APLIKASI PINTAR   ||")
